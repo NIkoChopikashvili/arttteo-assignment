73bb28d2b2d221a45009119b23c329f6aa935086
NikoChopikashvili <nikochopikashvili@yahoo.com>
2024-06-14 19:03:24 +0400

    fix: docker compose

--- src/databse/db-setup.js
+++ src/databse/db-setup.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.ROLE,
   process.env.PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: !process.env.DISABLE_SQL_LOGGER,
     dialectModule: require('pg'),
